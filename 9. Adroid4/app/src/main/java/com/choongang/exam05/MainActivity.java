d6b5d39438de3626ef6a5b6a6ee7d7b6be8e003c
dani <bin0696@naver.com>
2024-02-21 16:01:36 +0900

    0221 안드로이드 수업

---
+++ 9. Adroid4/app/src/main/java/com/choongang/exam05/MainActivity.java
@@ -0,0 +1,14 @@
+package com.choongang.exam05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ex01);
+    }
+}
--- 9. Android3/app/src/main/java/com/choongang/exam03/MainActivity.java
+++ 9. Android3/app/src/main/java/com/choongang/exam03/MainActivity.java
@@ -3,12 +3,44 @@ package com.choongang.exam03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[] buttons;
+    private FrameLayout[] contents;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttons = new Button[3];
+        buttons[0] = findViewById(R.id.button);
+        buttons[1] = findViewById(R.id.button2);
+        buttons[2] = findViewById(R.id.button3);
+
+        contents = new FrameLayout[3];
+        contents[0] = findViewById(R.id.content1);
+        contents[1] = findViewById(R.id.content2);
+        contents[2] = findViewById(R.id.content3);
+
+        for(int i=0; i<buttons.length; i++){
+            int num = i;
+            buttons[i].setOnClickListener(v->{
+                showContent(num);
+            });
+        }
+    }
+    private void showContent(int i){
+        for(FrameLayout content : contents){
+            content.setVisibility(View.GONE);
+        }
+        Log.i("BUTTON", String.valueOf(i));
+        contents[i].setVisibility(View.VISIBLE);
     }
 }
